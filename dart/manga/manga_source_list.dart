8505889996d3c484aaf3d615c09abf2e900c4b5c
cgcnl <cgcnl@localhost.local>
2025-02-17 10:59:58 +0700

    remove all sources

--- dart/manga/manga_source_list.dart
+++ dart/manga/manga_source_list.dart
@@ -1,16 +1,4 @@
 import '../../model/source.dart';
-import 'multisrc/madara/sources.dart';
-import 'multisrc/mangabox/sources.dart';
-import 'multisrc/mangareader/sources.dart';
-import 'multisrc/mmrcms/sources.dart';
-import 'multisrc/nepnep/sources.dart';
-import 'src/en/mangahere/source.dart';
 
 List<Source> dartMangasourceList = [
-  ...madaraSourcesList,
-  ...mangareaderSourcesList,
-  ...mmrcmsSourcesList,
-  mangahereSource,
-  ...nepnepSourcesList,
-  ...mangaboxSourcesList
 ];
